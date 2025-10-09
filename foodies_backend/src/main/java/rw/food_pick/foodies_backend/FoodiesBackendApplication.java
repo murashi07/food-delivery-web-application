403690edcb4cd0ec7a2595fb8dec4a871552f22c
murashi07 <yvanmurashi@gmail.com>
2025-10-09 23:59:43 +0200

    backend first commit

---
+++ foodies_backend/src/main/java/rw/food_pick/foodies_backend/FoodiesBackendApplication.java
@@ -0,0 +1,13 @@
+package rw.food_pick.foodies_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiesBackendApplication.class, args);
+	}
+
+}
